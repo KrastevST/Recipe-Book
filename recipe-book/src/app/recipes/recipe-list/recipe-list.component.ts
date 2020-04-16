48d07d50ea4bffdefc1c2ca24440e968955d3277
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-16 23:20:53 +0300

    fixed recipe list visualisation

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg'),
+    new Recipe('A test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg')
+  ]
 
   constructor() { }
 
