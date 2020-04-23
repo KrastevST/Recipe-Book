96627d2c2bceecfc2bc06750435331571cfb7594
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-23 21:54:09 +0300

    recipes can now be selected from the list

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -8,9 +8,10 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg'),
-    new Recipe('A test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg')
+    new Recipe('Another test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg')
   ]
 
   constructor() { }
@@ -18,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
