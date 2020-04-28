e89ee8327e842b5e43d17e6f083441858c5eaf92
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-28 16:43:10 +0300

    added cross-component communication service

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core'
+import { Injectable, EventEmitter } from '@angular/core'
 
 import { Recipe } from './recipe.model'
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>()
+  
   private recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg'),
     new Recipe('Another test Recipe', 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg')
