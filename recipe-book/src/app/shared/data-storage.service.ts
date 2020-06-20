09649021150b63c6a94430dff2097a300ce9b1a5
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-06-21 00:25:08 +0300

    enabled data saving via header dropdown

--- recipe-book/src/app/shared/data-storage.service.ts
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private recipeService: RecipeService) {}
 
-  storeRecipes
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes()
+    this.http
+      .put(
+        'https://recipe-book-f39cb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
 }
