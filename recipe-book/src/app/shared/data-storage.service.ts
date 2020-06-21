41b731f3da347d91737e05829bd2125c47048a8a
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-06-21 19:36:06 +0300

    enabled data fetching via header dropdown

--- recipe-book/src/app/shared/data-storage.service.ts
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
@@ -16,6 +17,14 @@ export class DataStorageService {
       )
       .subscribe(response => {
         console.log(response);
+      })
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://recipe-book-f39cb.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes)
       })
   }
 }
