aea0723876fc217e1d67d1562d2676003bb387fd
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-07-02 18:29:16 +0300

    auth form now sends a sign up request

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
   
   signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyBqLDRd0weqrY4oRJ7piXq2ThNMeOBOi-Y',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBqLDRd0weqrY4oRJ7piXq2ThNMeOBOi-Y',
       {
         email: email,
         password: password,
