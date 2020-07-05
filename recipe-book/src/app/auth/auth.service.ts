38f6d791eafa323bb5cfa8d9ce556ebfd7a2590f
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-07-05 15:38:52 +0300

    enabled sending of login requests

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string
   idToken: string
   email: string
   refreshToken: string
   expiresIn: string
   localId: string
+  registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -37,4 +38,16 @@ export class AuthService {
       return throwError(errorMessage)
     }))
   }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqLDRd0weqrY4oRJ7piXq2ThNMeOBOi-Y',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    )
+  }
+
 }
