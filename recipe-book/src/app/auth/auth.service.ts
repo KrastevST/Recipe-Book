0977d6e039e9da6eca23b027916ce98eb7b16935
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-07-09 04:16:59 +0300

    UI now reflects auth state

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -32,15 +32,17 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError),
-      tap(resData => {
-        this.handleAuthentication(
-          resData.email,
-          resData.localId,
-          resData.idToken,
-          +resData.expiresIn
-        )
-      }))
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        })
+      )
   }
 
   login(email: string, password: string) {
@@ -53,20 +55,27 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError))
+      .pipe(catchError(this.handleError),
+      tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        })
+      )
   }
 
-  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
-        const expirationDate = new Date(
-          new Date().getTime() + expiresIn * 1000
-        )
-        const user = new User(
-          email,
-          userId,
-          token,
-          expirationDate
-        )
-        this.user.next(user)
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
+    const user = new User(email, userId, token, expirationDate)
+    this.user.next(user)
   }
 
   private handleError(errorRes: HttpErrorResponse) {
