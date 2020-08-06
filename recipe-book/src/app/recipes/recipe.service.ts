104c72134e3ff95d49b96ac83c1c244422890f7f
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-08-06 13:07:21 +0300

    added multiple actions

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,44 +1,50 @@
 import { Injectable, } from '@angular/core'
 import { Subject } from 'rxjs'
+import { Store } from '@ngrx/store'
 
 import { Recipe } from './recipe.model'
 import { Ingredient } from '../shared/ingredient.model'
 import { ShoppingListService } from '../shopping-list/shopping-list.service'
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions'
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
   
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Chicken Soup',
-      'Tasty chicken soup with nettle',
-      'https://pinchofyum.com/wp-content/uploads/Lemon-Chicken-Soup-Square.jpg',
-     [
-       new Ingredient('Chicken', 1),
-       new Ingredient('Yoghurt', 1),
-       new Ingredient('Egg', 3),
-       new Ingredient('Nettle', 1),
-       new Ingredient('Lemon', 1),
-       new Ingredient('Carrot', 4),
-       new Ingredient('Potatoe', 3),
-       new Ingredient('Rice', 1),
-       new Ingredient('Onion', 2),
-     ]), 
-    new Recipe(
-      'Rice Fried Chicken',
-      'Chicken meat with rice and veggies',
-      'https://www.tasteofhome.com/wp-content/uploads/2018/05/Brown-Rice-Salad-with-Grilled-Chicken_exps16302_CX1995537B04_08_1b_RMS-696x696.jpg',
-      [
-        new Ingredient('Chicken', 1),
-        new Ingredient('Carrot', 3),
-        new Ingredient('Rice', 1),
-        new Ingredient('Onion', 2),
-        new Ingredient('Peas', 1),
-      ]),
-  ]
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Chicken Soup',
+  //     'Tasty chicken soup with nettle',
+  //     'https://pinchofyum.com/wp-content/uploads/Lemon-Chicken-Soup-Square.jpg',
+  //    [
+  //      new Ingredient('Chicken', 1),
+  //      new Ingredient('Yoghurt', 1),
+  //      new Ingredient('Egg', 3),
+  //      new Ingredient('Nettle', 1),
+  //      new Ingredient('Lemon', 1),
+  //      new Ingredient('Carrot', 4),
+  //      new Ingredient('Potatoe', 3),
+  //      new Ingredient('Rice', 1),
+  //      new Ingredient('Onion', 2),
+  //    ]), 
+  //   new Recipe(
+  //     'Rice Fried Chicken',
+  //     'Chicken meat with rice and veggies',
+  //     'https://www.tasteofhome.com/wp-content/uploads/2018/05/Brown-Rice-Salad-with-Grilled-Chicken_exps16302_CX1995537B04_08_1b_RMS-696x696.jpg',
+  //     [
+  //       new Ingredient('Chicken', 1),
+  //       new Ingredient('Carrot', 3),
+  //       new Ingredient('Rice', 1),
+  //       new Ingredient('Onion', 2),
+  //       new Ingredient('Peas', 1),
+  //     ]),
+  // ]
+  private recipes: Recipe[] = []
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(
+    private slService: ShoppingListService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes
@@ -54,7 +60,8 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients)
+    // this.slService.addIngredients(ingredients)
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients))
   }
 
   addRecipe(recipe: Recipe) {
