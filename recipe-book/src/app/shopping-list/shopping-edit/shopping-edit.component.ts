1fb682a44b761d5571bfeb97b3397f48523d24cf
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-08-08 00:50:51 +0300

    refactor

--- recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -17,7 +17,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f') slForm: NgForm
   subscription: Subscription
   editMode = false
-  editedItemIndex: number
   editedItem: Ingredient
 
   constructor(
@@ -26,18 +25,18 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-    this.subscription = this.shoppingListService.startedEditing
-      .subscribe(
-        (index: number) => {
-          this.editedItemIndex = index
-          this.editMode = true
-          this.editedItem = this.shoppingListService.getIngredient(index)
-          this.slForm.setValue({
-            name: this.editedItem.name,
-            amount: this.editedItem.amount,
-          })
-        }
-      )
+    this.subscription = this.store.select('shoppingList').subscribe(stateData => {
+      if (stateData.editedIngredientIndex > -1) {
+        this.editMode = true
+        this.editedItem = stateData.editedIngredient
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        })
+      } else {
+        this.editMode = false
+      }
+    })
   }
 
   onSubmit(form: NgForm) {
@@ -46,7 +45,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     if (this.editMode) {
       // this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient)
       this.store.dispatch(
-        new ShoppingListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient})
+        new ShoppingListActions.UpdateIngredient(newIngredient)
       )
     } else {
       // this.shoppingListService.addIngredient(newIngredient)
@@ -59,17 +58,19 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onClear() {
     this.slForm.reset()
     this.editMode = false
+    this.store.dispatch(new ShoppingListActions.StopEdit())
   }
 
   onDelete() {
     // this.shoppingListService.deleteIngredient(this.editedItemIndex)
     this.store.dispatch(
-      new ShoppingListActions.DeleteIngredient(this.editedItemIndex)
+      new ShoppingListActions.DeleteIngredient()
     )
     this.onClear()
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe()
+    this.store.dispatch(new ShoppingListActions.StopEdit())
   }
 }
